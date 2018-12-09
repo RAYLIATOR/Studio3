87bbec26628cfeb02665873daeb9a75d5e334b81
Aaron <aaronferns1@yahoo.com>
2018-12-09 17:04:37 +0400

    Week 9, Photon test

---
+++ Studio3/Assets/Aaron/Scripts/NetworkingMain.cs
@@ -0,0 +1,152 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NetworkingMain : MonoBehaviourPunCallbacks
+{
+    MenuManager menuManager;
+    public InputField userInputField;
+    public InputField roomInputField;
+    PlayersManager pM;
+    string roomName;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        menuManager = FindObjectOfType<MenuManager>();
+        pM = FindObjectOfType<PlayersManager>();
+    }
+
+    void Update()
+    {
+        if(PhotonNetwork.CurrentRoom != null)
+            print("player Count" + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+    
+    public void SelectPlayer1()
+    {
+        pM.onlineColor = 1;
+    }
+    
+    public void SelectPlayer2()
+    {
+        pM.onlineColor = 2;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Successfully connected to server.");
+        PhotonNetwork.JoinRandomRoom(null, 0);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        base.OnJoinRandomFailed(returnCode, message);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.IsVisible = true;
+        roomOptions.MaxPlayers = 4;
+
+        TypedLobby typedLobby = new TypedLobby();
+        typedLobby.Name = "Lobby 1";
+        typedLobby.Type = LobbyType.Default;
+
+        roomName = roomInputField.text;
+        PhotonNetwork.CreateRoom(roomName, roomOptions, typedLobby);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        base.OnCreateRoomFailed(returnCode, message);
+        PhotonNetwork.JoinRoom(roomName);
+
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        print("Created Room");
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        print("Joined Lobby");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        print(PhotonNetwork.LocalPlayer.NickName + " has joined the room");
+
+       if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            pM.onlineID = 1;
+            PhotonNetwork.LoadLevel(1);
+        }
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            pM.onlineID = 2;
+            PhotonNetwork.LoadLevel(1);
+            Instantiator instantiator = FindObjectOfType<Instantiator>();
+            instantiator.player1.GetComponent<PlayerController>().Init();
+            instantiator.player2.GetComponent<PlayerController>().Init();
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        print(newPlayer.NickName + " has joined the room");
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        base.OnPlayerLeftRoom(otherPlayer);
+        menuManager.YouWin();
+        Invoke("LeaveRoom", 1);
+        print(otherPlayer.NickName + " has left the room");
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        print(PhotonNetwork.LocalPlayer.NickName + " has left the Room");
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        PhotonNetwork.LoadLevel(0);
+    }
+
+    public override void OnLeftLobby()
+    {
+        base.OnLeftLobby();
+    }
+
+    public void ConnectToServer()
+    {
+        PhotonNetwork.NickName = userInputField.text;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ Studio3/Assets/Aaron/Scripts/PlayerController.cs
@@ -0,0 +1,191 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour, IPunObservable
+{
+    Animator animator;
+    PhotonView pv;
+    PlayersManager pM;
+    MenuManager menuManager;
+    float health;
+    //float enemyHealth;
+    Image myHealthBar;
+    Image enemyHealthBar;
+    float minX;
+    float maxX;
+    Rigidbody rb;
+    float force;
+    int lightLevel;
+    int heavyLevel;
+    GameObject opponent;
+    PlayerController opponentScript;
+    Instantiator instantiator;
+    public bool twoPlayersConnected;
+
+    void Start ()
+    {
+        instantiator = FindObjectOfType<Instantiator>();
+        menuManager = FindObjectOfType<MenuManager>();
+        lightLevel = 1;
+        heavyLevel = 1;
+        //rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        force = 50f;
+        minX = -3.5f;
+        maxX = 3.5f;
+        pv = GetComponent<PhotonView>();
+        pM = FindObjectOfType<PlayersManager>();
+        health = 100;
+        //enemyHealth = 100;
+	}
+	
+    public void Init()
+    {
+        if (pM.onlineID == 1)
+        {
+            opponent = instantiator.player2;
+            opponentScript = opponent.GetComponent<PlayerController>();
+            transform.Rotate(0, 90, 0);
+            myHealthBar = GameObject.FindGameObjectWithTag("hb1").GetComponent<Image>();
+            //enemyHealthBar = GameObject.FindGameObjectWithTag("hb2").GetComponent<Image>();
+        }
+        else if (pM.onlineID == 2)
+        {
+            opponent = instantiator.player1;
+            opponentScript = opponent.GetComponent<PlayerController>();
+            transform.Rotate(0, -90, 0);
+            myHealthBar = GameObject.FindGameObjectWithTag("hb2").GetComponent<Image>();
+            //enemyHealthBar = GameObject.FindGameObjectWithTag("hb1").GetComponent<Image>();
+        }
+    }
+
+	void Update ()
+    {
+        if (pv.IsMine)
+        {
+            MovePlayer();
+            Fight();
+            //print(myHealthBar.fillAmount);
+            //print(health + " " + pv.Owner.NickName);
+            if(health <= 0)
+            {
+                menuManager.GameOver();
+            }
+        }
+        if (myHealthBar != null)
+        {
+            myHealthBar.fillAmount = health / 100;
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.Serialize(ref health);
+        }
+        else
+        {
+             stream.Serialize(ref health);
+        }
+    }
+
+    void MovePlayer()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.1f;
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    void Fight()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (heavyLevel == 1)
+            {
+                animator.SetTrigger("Heavy1");
+                opponentScript.TakeDamage(10);
+            }
+            else if (heavyLevel == 2)
+            {
+                animator.SetTrigger("Heavy2");
+                opponentScript.TakeDamage(10);
+            }
+            else if (heavyLevel == 3)
+            {
+                animator.SetTrigger("Heavy3");
+                opponentScript.TakeDamage(10);
+            }
+            if (heavyLevel < 3)
+            {
+                heavyLevel += 1;
+            }
+            else
+            {
+                heavyLevel = 1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (lightLevel == 1)
+            {
+                animator.SetTrigger("Light1");
+                opponentScript.TakeDamage(5);
+            }
+            else if (lightLevel == 2)
+            {
+                animator.SetTrigger("Light2");
+                opponentScript.TakeDamage(5);
+            }
+            else if (lightLevel == 3)
+            {
+                animator.SetTrigger("Light3");
+                opponentScript.TakeDamage(5);
+            }
+            if (lightLevel < 3)
+            {
+                lightLevel += 1;
+            }
+            else
+            {
+                lightLevel = 1;
+            }
+        }
+    }
+
+    //Mobile Controls below
+
+    public void MoveLeft()
+    {
+        if (transform.position.x > minX)
+        {
+            rb.AddForce(Vector3.left * force);
+        }
+    }
+
+    public void MoveRight()
+    {
+        if (transform.position.x < maxX)
+        {
+            rb.AddForce(Vector3.right * force);
+        }
+    }
+
+    public void Stop()
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
