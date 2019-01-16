6f2db463bccbe8ccb11b0adcf7d1c64b22025ed1
Ajay Rawat <a.rawat@saedubai.com>
2019-01-16 13:04:52 +0400

    Added the combo attack

--- Studio3/Assets/Ajay/Scripts/OfflineController.cs
+++ Studio3/Assets/Ajay/Scripts/OfflineController.cs
@@ -13,6 +13,8 @@ public class OfflineController : MonoBehaviour
 
     public Image P1HealthBar;
     public Image P2HealthBar;
+    int lightAttack;
+    int heavyAttack;
 
     void Start()
     {
@@ -20,6 +22,9 @@ public class OfflineController : MonoBehaviour
         animator = GetComponent<Animator>();
         Player1 = GameObject.FindGameObjectWithTag("p1");
         Player2 = GameObject.FindGameObjectWithTag("p2");
+
+        lightAttack = 1;
+        heavyAttack = 1;
 
         //Player1.transform.Rotate(0, 90, 0);
         P1HealthBar = GameObject.FindGameObjectWithTag("hb1").GetComponent<Image>(); 
@@ -54,13 +59,53 @@ public class OfflineController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.W) && Player1 != null)
         {
-            Player1.GetComponent<Animator>().SetTrigger("Heavy1");
+            if(heavyAttack == 1)
+            {
+                Player1.GetComponent<Animator>().SetTrigger("Heavy1");
+            }
+            else if (heavyAttack == 2)
+            {
+                Player1.GetComponent<Animator>().SetTrigger("Heavy2");
+            }
+            else if (heavyAttack == 3)
+            {
+                Player1.GetComponent<Animator>().SetTrigger("Heavy3");
+            }
+
+            if(heavyAttack < 3)
+            {
+                heavyAttack += 1;
+            }
+            else
+            {
+                heavyAttack = 1;
+            }
             P2HealthBar.fillAmount -= moveSpeed;
         }
 
         if (Input.GetKeyDown(KeyCode.S) && Player1 != null)
         {
-            Player1.GetComponent<Animator>().SetTrigger("Light1");
+            if(lightAttack == 1)
+            {
+                Player1.GetComponent<Animator>().SetTrigger("Light1");
+            }
+            else if (lightAttack == 2)
+            {
+                Player1.GetComponent<Animator>().SetTrigger("Light2");
+            }
+            else if (lightAttack == 3)
+            {
+                Player1.GetComponent<Animator>().SetTrigger("Light3");
+            }
+
+            if(lightAttack < 3)
+            {
+                lightAttack += 1;
+            }
+            else
+            {
+                lightAttack = 1;
+            }
             P2HealthBar.fillAmount -= moveSpeed;
         }
 
@@ -76,12 +121,52 @@ public class OfflineController : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.I) && Player2 != null)
         {
-            Player2.GetComponent<Animator>().SetTrigger("Heavy1");
+             if(heavyAttack == 1)
+            {
+                Player2.GetComponent<Animator>().SetTrigger("Heavy1");
+            }
+            else if (heavyAttack == 2)
+            {
+                Player2.GetComponent<Animator>().SetTrigger("Heavy2");
+            }
+            else if (heavyAttack == 3)
+            {
+                Player2.GetComponent<Animator>().SetTrigger("Heavy3");
+            }
+
+            if(heavyAttack < 3)
+            {
+                heavyAttack += 1;
+            }
+            else
+            {
+                heavyAttack = 1;
+            }
             P1HealthBar.fillAmount -= moveSpeed;
         }
         if (Input.GetKeyDown(KeyCode.K) && Player2 != null)
         {
-            Player2.GetComponent<Animator>().SetTrigger("Light1");
+            if(lightAttack == 1)
+            {
+                Player2.GetComponent<Animator>().SetTrigger("Light1");
+            }
+            else if (lightAttack == 2)
+            {
+                Player2.GetComponent<Animator>().SetTrigger("Light2");
+            }
+            else if (lightAttack == 3)
+            {
+                Player2.GetComponent<Animator>().SetTrigger("Light3");
+            }
+
+            if(lightAttack < 3)
+            {
+                lightAttack += 1;
+            }
+            else
+            {
+                lightAttack = 1;
+            }
             P1HealthBar.fillAmount -= moveSpeed;
         }
     }
